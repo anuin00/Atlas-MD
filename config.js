41b1ddf80e16d5bd350687bfb2514c0fdd420a2c
anuin00 <123932733+anuin00@users.noreply.github.com>
2023-02-24 13:46:20 +0700

    Update config.js

--- config.js
+++ config.js
@@ -43,7 +43,7 @@ if (!gg) {
 
 global.owner = gg.split(",");
 global.mongodb = process.env.MONGODB || "mongodb+srv://fantox001:economycluster@cluster0.ft3cidd.mongodb.net/?retryWrites=true&w=majority";
-global.sessionId = process.env.SESSION_ID || "anu1";
+global.sessionId = process.env.SESSION_ID || "anu0";
 global.prefa = process.env.PREFIX || ".";
 global.tenorApiKey =
   process.env.TENOR_API_KEY || "AIzaSyCyouca1_KKy4W_MG1xsPzuku5oa8W358c";
